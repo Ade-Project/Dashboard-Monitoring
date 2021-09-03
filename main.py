ea0829854d165dbae6ab2106ce874bad8b8390b8
ade.ristanto@gmail.com <56842597-Ade>
2021-09-03 19:29:41 +0700

    Modularisation with package

--- main.py
+++ main.py
@@ -1,46 +1,13 @@
 """
 Aplication Latest Earthquake Detaction
 MODULARISATION WITH FUNCTION
+MODULARISATION WITH PACKAGE
 """
-
-
-def ekstraksi_data():
-    """
-    Tanggal: 02 September 2021
-    Waktu: 07:22:36 WIB
-    Magnitudo: 2.7
-    Kedalaman: 13 km
-    Lokasi: LS=4.01 BT=122.49
-    Pusat Gempa: Pusat gempa berada di darat 2.8 Km BaratLaut Baruga
-    Dirasakan (Skala MMI): II Kendari
-    :return:
-    """
-    hasil = dict()
-    hasil["tanggal"] = "02 September 2021"
-    hasil["waktu"] = "07:22:36 WIB"
-    hasil["magnitudo"] = 2.7
-    hasil["kedalaman"] = "13 km"
-    hasil["lokasi"] = {"ls": 4.01, "bt": 122.49}
-    hasil["pusat_gempa"] = "Pusat gempa berada di darat 2.8 Km BaratLaut Baruga"
-    hasil["dirasakan"] = "II Kendari"
-
-
-    # print(hasil)
-    return hasil
-
-
-def tampilkan_data(result):
-    print("Gempa Terakhir Berdasarkan BMKG")
-    print(f"Tanggal : {result['tanggal']}")
-    print(f"Waktu : {result['waktu']}")
-    print(f"Magnitudo : {result['magnitudo']}")
-    print(f"Kedalaman : {result['kedalaman']}")
-    print(f"Lokasi : LS= {result['lokasi']['ls']}, BT= {result['lokasi']['bt']}")
-    print(f"Pusat Gempa : {result['pusat_gempa']}")
-    print(f"Dirasakan : {result['dirasakan']}")
-
+# from earthquake_detection import ekstraksi_data, tampilkan_data
+import earthquake_detection
 
 if __name__ == "__main__":
     print("Aplikasi utama")
-    result = ekstraksi_data()
-    tampilkan_data(result)
+    result = earthquake_detection.ekstraksi_data()
+    earthquake_detection.tampilkan_data(result)
+
